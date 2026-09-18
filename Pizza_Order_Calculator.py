b8e2a26a9856c73fec7cee25de93d8dbb4068e02
Kennedy Walker <kwalk94@student.cscc.edu>
2026-09-18 14:07:29 -0400

    push to github

---
+++ Pizza_Order_Calculator.py
@@ -0,0 +1,17 @@
+""" Pizza Order Calculator
+Kennedy Walker
+Date: 9/18/2026
+Reason: This program is to calculate the prcie of the pizza after the customer orders"""
+
+print ("Welcome to Kennedy's Pizza")
+
+# Using if, elif, then statement to add price for the size of the pizza
+size = input ("What size pizza would you like? Small, Medium, or Large")
+
+if size = "Small":
+    price = 10
+elif size = "Medium":
+     price = 12
+elif size = "Large":
+    price = 15
+else print ("Sorry that is not an option")
