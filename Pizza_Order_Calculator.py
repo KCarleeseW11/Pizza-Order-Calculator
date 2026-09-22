76377208fa71f9df1d637cdb28968067ad672d9b
Kennedy Walker <kwalk94@student.cscc.edu>
2026-09-22 11:01:14 -0400

    Added prices of toppings

--- Pizza_Order_Calculator.py
+++ Pizza_Order_Calculator.py
@@ -14,8 +14,15 @@ elif size == "Medium":
      price = 12
 elif size == "Large":
     price = 15
-else print ("Sorry that is not an option")
+else:
+     print ("Sorry that is not an option")
 
 #Adding toppin choice using a List
-print ("What toppings would you like on your pizza?")
 toppings = [ "Cheese only", "Pepperponi", "Sausauge", "Bacon"]
+print ("The availble toppings are :",toppings )
+choice = input("Select one topping you would like for your Pizza")
+#adding price of toppings 
+ if choice == "Cheese only":
+    with_topping =0
+else:
+with_topping = 2
