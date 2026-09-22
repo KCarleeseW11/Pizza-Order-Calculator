a3ff73749f8d748d576dbbd39be5374ab28869b9
Kennedy Walker <kwalk94@student.cscc.edu>
2026-09-22 12:33:35 -0400

    Debugged program and fixed errors

--- Pizza_Order_Calculator.py
+++ Pizza_Order_Calculator.py
@@ -8,25 +8,26 @@ print ("Welcome to Kennedy's Pizza")
 # Using if, elif, then statement to add price for the size of the pizza
 size = input ("What size pizza would you like? Small, Medium, or Large")
 
+
+while size != "Small" and size != "Medium" and size != "Large" :
+    print("That is not an option")
 if size == "Small":
     price = 10
 elif size == "Medium":
      price = 12
 elif size == "Large":
-    price = 15
-else:
-     print ("Sorry that is not an option")
-
+    price = 15 
+      
 #Adding toppin choice using a List
 toppings = [ "Cheese only", "Pepperponi", "Sausauge", "Bacon"]
 print ("The availble toppings are :",toppings )
 choice = input("Select one topping you would like for your Pizza")
 #adding price of toppings 
 
- if choice == "Cheese only":
+if choice == "Cheese only":
     with_topping =0
 else:
-with_topping = 2
+    with_topping = 2
 
 final_price = price + with_topping
 print (" The final cost of your order will be :$", final_price)
